731182ab38e33d444a890273dc20c5511ff53e9d
imranbs93 <89394899+imran-myvenuelive@users.noreply.github.com>
2022-03-23 14:28:12 +0600

    attendence UI

--- src/screen/timeSheets/Attendances.js
+++ src/screen/timeSheets/Attendances.js
@@ -2,28 +2,33 @@ import * as React from 'react';
 import DataTable from '../../components/dataTable/DataTable';
 import {SafeAreaView} from 'react-native';
 import {ScaledSheet} from 'react-native-size-matters';
+import {View} from 'native-base';
+import SearchBox from '../../components/searchBox/SearchBox';
 
 const Attendances = () => {
   const tableHead = {
     tableHead: [
-      'Head',
-      'Head2',
-      'Head3',
-      'Head4',
-      'Head5',
-      'Head6',
-      'Head7',
-      'Head8',
-      'Head9',
+      'Sl',
+      'Employee',
+      'Date',
+      'Status',
+      'Clock In',
+      'IN LC',
+      'Clock Out',
+      'OUT LC',
+      'Late',
+      'Early Leaving',
+      'Overtime',
+      'Total Work',
     ],
-    widthArr: [40, 60, 80, 100, 120, 140, 160, 180, 200],
+    widthArr: [50, 140, 80, 100, 120, 140, 160, 180, 200, 200, 200, 200],
   };
 
   const data = [];
 
   for (let i = 0; i < 30; i += 1) {
     const dataRow = [];
-    for (let j = 0; j < 9; j += 1) {
+    for (let j = 0; j < 12; j += 1) {
       dataRow.push(`${i}${j}`);
     }
     data.push(dataRow);
@@ -31,7 +36,10 @@ const Attendances = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <DataTable tableHead={tableHead} data={data} />
+      <View style={styles.search}>
+        <SearchBox />
+      </View>
+      <DataTable tableHead={tableHead} data={data} headerColour={'#00695c'} />
     </SafeAreaView>
   );
 };
@@ -39,6 +47,11 @@ const Attendances = () => {
 const styles = ScaledSheet.create({
   container: {
     flex: 1,
+  },
+  search: {
+    paddingLeft: 17,
+    paddingRight: 17,
+    backgroundColor: '#fff',
   },
 });
 
