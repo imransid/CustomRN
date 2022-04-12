5244061c72e196d7e4fcb144195e885b1184609c
jessanKhan <39450312+jessanKhan@users.noreply.github.com>
2022-04-12 08:31:42 +0600

    Immigration data add

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,9 +1,10 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 import ApiSetupReducer from './ApiSetupReducer';
 import UserReducer from './UserReducer';
 import Setting from './SettingReducer';
 import Asset from './Asset';
 import Social from './Social.reducer'
+import Immigration from './Immigration.reducer'
 
 export default combineReducers({
   api: ApiSetupReducer,
@@ -11,4 +12,5 @@ export default combineReducers({
   setting: Setting,
   asset: Asset,
   social: Social,
+  immigration: Immigration
 });
