7f3cddad8267f33803041ff5bacb4558a9ecdc98
imranbs93 <89394899+imran-myvenuelive@users.noreply.github.com>
2022-04-19 19:43:02 +0600

    Profile api connect

--- src/screen/Profile/WorkExperience.js
+++ src/screen/Profile/WorkExperience.js
@@ -1,20 +1,28 @@
 import React, {useState, useEffect} from 'react';
-import {SafeAreaView, ScrollView, Modal, View} from 'react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  Modal,
+  View,
+  ToastAndroid,
+} from 'react-native';
 
+import AwesomeAlert from 'react-native-awesome-alerts';
 import TableCard from '../../components/TableCard/TableCard';
 import {ScaledSheet} from 'react-native-size-matters';
 import CustomModal from '../../components/CustomModal/CustomModal';
 import SearchBox from '../../components/searchBox/SearchBox';
-import {_postApiFetch} from '../../services/Services';
+import {_postApiFetch, _postApiADD} from '../../services/Services';
+
 import CustomIndicator from '../../components/CustomIndicator/CustomIndicator';
 import PlusButton from '../../components/plusButton';
 import {useSelector} from 'react-redux';
-
 import useFetchData from '../../components/HOC/withGetData';
 
 const WorkExperience = () => {
   const [modalVisible, setModalVisible] = useState(false);
   const id = useSelector(state => state.user.userAllData.id);
+  const com_id = useSelector(state => state.user.userAllData.com_id);
 
   let data = useFetchData(
     [['work_experience_employee_id', id]],
@@ -23,16 +31,192 @@ const WorkExperience = () => {
   );
 
   const [documentData, setDocumentData] = useState([]);
+  const [documentType, setDocumentType] = useState('');
   const [documentLoader, setDocumentLoader] = useState(false);
+  const [infoValue, setInfoValue] = useState([]);
+  const [showAlert, setShowAlert] = useState(false);
+
+  // type
+  const [type, setType] = useState('');
 
   useEffect(() => {
     try {
       data[1] !== documentLoader ? setDocumentLoader(data[1]) : null;
-      data[0].length !== documentData.length ? setDocumentData(data[0]) : null;
+      documentData.length === 0 ? setDocumentData(data[0]) : null;
     } catch (err) {
       console.log('Error in useEffect ', err);
     }
   }, [data, documentLoader, documentData]);
+
+  const OnEdit = async (info, type) => {
+    setModalVisible(false);
+
+    let filterInfo = info.filter(e => {
+      if (
+        e[0] !== 'work_experience_employee_id' &&
+        e[0] !== 'work_experience_com_id'
+      ) {
+        return e;
+      }
+    });
+
+    let parmZ = [];
+
+    // when need to check attachment and type
+
+    for (let i = 0; i < filterInfo.length; i++) {
+      if (filterInfo[i][0] === 'document_type') {
+        if (filterInfo[i][1] !== undefined) {
+          parmZ.push(filterInfo[i]);
+        } else {
+          filterInfo[i][1] = documentType;
+          console.log('infoValue', documentType);
+          parmZ.push(filterInfo[i]);
+        }
+      } else if (filterInfo[i][0] === 'document_file') {
+        if (typeof filterInfo[i][1] !== 'string') {
+          parmZ.push(filterInfo[i]);
+        }
+      } else {
+        parmZ.push(filterInfo[i]);
+      }
+    }
+
+    let parm = {
+      bodyData: parmZ,
+      uri: 'work-experience-update',
+    };
+
+    const result = await _postApiFetch(parm);
+
+    result.status ? setDocumentData(result.data) : null;
+
+    let msg = result.status
+      ? type === 'edit'
+        ? 'Update Successfully'
+        : 'Save Successfully'
+      : 'Failed Please Check Again.!';
+
+    showToastWithGravityAndOffset(msg);
+  };
+
+  const showToastWithGravityAndOffset = msg => {
+    ToastAndroid.showWithGravityAndOffset(
+      msg,
+      ToastAndroid.LONG,
+      ToastAndroid.BOTTOM,
+      25,
+      50,
+    );
+  };
+
+  const onPressEdit = data => {
+    setModalVisible(true);
+
+    setType('edit');
+
+    let objectData = Object.entries(data);
+
+    setDocumentType(data.document_type);
+
+    let finalData = objectData.filter(e => {
+      if (e[0] === 'created_at' || e[0] === 'updated_at') {
+      } else {
+        e[2] = e[0].toUpperCase().replaceAll('_', ' ');
+        return e;
+      }
+    });
+
+    setInfoValue(finalData);
+  };
+
+  const OnAddNow = () => {
+    setType('add');
+
+    let objectData = [
+      ['work_experience_com_id', com_id.toString(), 'work_experience_com_id'],
+      [
+        'work_experience_employee_id',
+        id.toString(),
+        'work_experience_employee_id',
+      ],
+      ['work_experience_company_name', '', 'work_experience_company_name'],
+      ['work_experience_from_date', 'Select Date', 'work_experience_from_date'],
+      ['work_experience_to_date', 'Select Date', 'work_experience_to_date'],
+      ['work_experience_post', '', 'work_experience_post'],
+      ['work_experience_desc', '', 'work_experience_desc'],
+    ];
+
+    let finalData = objectData.filter(e => {
+      if (e[0] === 'created_at' || e[0] === 'updated_at') {
+      } else {
+        e[2] = e[0].toUpperCase().replaceAll('_', ' ');
+        return e;
+      }
+    });
+
+    setInfoValue(finalData);
+
+    setModalVisible(true);
+  };
+
+  const OnAddPress = async (info, type) => {
+    setModalVisible(false);
+    setDocumentLoader(true);
+
+    let parm = {
+      bodyData: info,
+      uri: 'work-experience-add',
+    };
+
+    const result = await _postApiADD(parm);
+
+    result.status ? setDocumentData(result.data) : null;
+
+    if (result.status) {
+      setDocumentData(result.data);
+      setDocumentLoader(false);
+    } else {
+      setDocumentLoader(false);
+    }
+
+    let msg = result.status
+      ? type === 'edit'
+        ? 'Update Successfully'
+        : 'Save Successfully'
+      : 'Failed Please Check Again.!';
+
+    showToastWithGravityAndOffset(msg);
+  };
+
+  const _onDelete = async info => {
+    setModalVisible(false);
+    setDocumentLoader(true);
+
+    let parm = {
+      bodyData: info,
+      uri: 'work-experience-delete',
+    };
+
+    const result = await _postApiADD(parm);
+
+    result.status ? setDocumentData(result.data) : null;
+
+    if (result.status) {
+      setDocumentData(result.data);
+      setDocumentLoader(false);
+    } else {
+      setDocumentLoader(false);
+    }
+
+    result.status ? setDocumentData(result.data) : null;
+
+    let msg = result.status
+      ? 'Deleted Successfully. !'
+      : 'Failed Please Check Again.!';
+
+    showToastWithGravityAndOffset(msg);
+  };
 
   return (
     <>
@@ -45,18 +229,40 @@ const WorkExperience = () => {
             onRequestClose={() => {
               setModalVisible(false);
             }}>
-            <CustomModal onPress={() => setModalVisible(false)} children />
+            <CustomModal
+              modalName={'Work Experience'}
+              type={type}
+              onValue={infoValue}
+              dropDownValue={[
+                {label: 'Other', value: 'Other'},
+                {label: 'Certificate', value: 'Certificate'},
+              ]}
+              onPress={(e, type) => {
+                if (type) {
+                  type === 'edit' ? OnEdit(e, type) : OnAddPress(e, type);
+                } else {
+                  setModalVisible(false);
+                }
+              }}
+              children
+            />
           </Modal>
           <View style={styles.search}>
             <SearchBox />
           </View>
-          {documentLoader && <CustomIndicator />}
-
-          {!documentLoader &&
+          {documentLoader ? (
+            <CustomIndicator />
+          ) : (
             documentData?.map((data, i) => (
               <TableCard
                 key={i}
                 sl={i + 1}
+                onEdit={() => onPressEdit(data)}
+                onDelete={() => {
+                  let val = [['id', data.id.toString(), 'ID']];
+                  _onDelete(val);
+                  setShowAlert(false);
+                }}
                 datas={[
                   {
                     title: 'Company Name',
@@ -67,15 +273,40 @@ const WorkExperience = () => {
                   {title: 'Position', value: data.work_experience_post},
                   {title: 'Description', value: data.work_experience_desc},
                 ]}
+                deleteButton={true}
+                buttonVisible={true}
                 variant="Immigration"
               />
-            ))}
+            ))
+          )}
+
+          <AwesomeAlert
+            show={showAlert}
+            showProgress={false}
+            title=""
+            message="Are You Sure Want To Delete it?"
+            closeOnTouchOutside={true}
+            closeOnHardwareBackPress={false}
+            showCancelButton={true}
+            showConfirmButton={true}
+            cancelText="No, cancel"
+            confirmText="Yes, delete it"
+            confirmButtonColor="#DD6B55"
+            onCancelPressed={() => {
+              // this.hideAlert();
+              setShowAlert(false);
+            }}
+            onConfirmPressed={() => {
+              console.log('id', infoValue);
+              // this.hideAlert();
+            }}
+          />
 
           {/* ))} */}
           {/* </TouchableOpacity> */}
         </SafeAreaView>
       </ScrollView>
-      <PlusButton OnPress={() => setModalVisible(true)} />
+      <PlusButton OnPress={() => OnAddNow()} />
     </>
   );
 };
