ada2e3307adfc4d3874f8e29c4a27b081cc2db83
imranbs93 <89394899+imran-myvenuelive@users.noreply.github.com>
2022-04-21 16:44:19 +0600

    RFID

--- src/screen/Profile/WorkExperience.js
+++ src/screen/Profile/WorkExperience.js
@@ -6,10 +6,9 @@ import {
   View,
   ToastAndroid,
 } from 'react-native';
-
+import RnPdf from '../../components/GenaratePdf';
 import AwesomeAlert from 'react-native-awesome-alerts';
 import TableCard from '../../components/TableCard/TableCard';
-import {ScaledSheet} from 'react-native-size-matters';
 import CustomModal from '../../components/CustomModal/CustomModal';
 import SearchBox from '../../components/searchBox/SearchBox';
 import {_postApiFetch, _postApiADD} from '../../services/Services';
@@ -18,6 +17,7 @@ import CustomIndicator from '../../components/CustomIndicator/CustomIndicator';
 import PlusButton from '../../components/plusButton';
 import {useSelector} from 'react-redux';
 import useFetchData from '../../components/HOC/withGetData';
+import styles from './Styles';
 
 const WorkExperience = () => {
   const [modalVisible, setModalVisible] = useState(false);
@@ -250,6 +250,9 @@ const WorkExperience = () => {
           <View style={styles.search}>
             <SearchBox />
           </View>
+          <View style={styles.pdfBox}>
+            <RnPdf Filename={'WorkExperience'} value={data[0]} />
+          </View>
           {documentLoader ? (
             <CustomIndicator />
           ) : (
@@ -311,102 +314,4 @@ const WorkExperience = () => {
   );
 };
 
-const styles = ScaledSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  search: {
-    paddingLeft: 17,
-    paddingRight: 17,
-    backgroundColor: '#fff',
-  },
-  container: {
-    backgroundColor: '#F2F2F2',
-  },
-  eventList: {
-    marginTop: 20,
-  },
-  listitem: {
-    padding: 10,
-    marginTop: 5,
-    marginBottom: 5,
-    flexDirection: 'row',
-  },
-  sl: {
-    flexDirection: 'column',
-  },
-  slno: {
-    fontSize: 50,
-    color: '#0099FF',
-    fontWeight: '600',
-  },
-  eventMonth: {
-    fontSize: 16,
-    color: '#0099FF',
-    fontWeight: '600',
-  },
-  poilcyContent: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    marginLeft: 10,
-    backgroundColor: '#FFFFFF',
-    padding: 10,
-    borderRadius: 10,
-  },
-  description: {
-    fontSize: 15,
-    color: '#646464',
-  },
-  policyTitle: {
-    fontSize: 18,
-    color: '#151515',
-  },
-  addedBy: {
-    fontSize: 16,
-    color: '#151515',
-  },
-  modal: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0,0,0,0.5)',
-  },
-  modalContent: {
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    padding: 10,
-    width: '80%',
-    height: '80%',
-  },
-  modalTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#151515',
-    marginBottom: 10,
-  },
-  modalText: {
-    fontSize: 15,
-    color: '#151515',
-    marginBottom: 10,
-  },
-  modalButton: {
-    backgroundColor: '#0099FF',
-    padding: 10,
-    marginTop: 10,
-    borderRadius: 10,
-  },
-  modalButtonText: {
-    fontSize: 20,
-    color: '#FFFFFF',
-    fontWeight: '600',
-  },
-  horizontal: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    padding: 10,
-  },
-  activityIndicator: {alignSelf: 'center', paddingVertical: '50%'},
-});
 export default WorkExperience;
